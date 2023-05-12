29f3b286ba1af8d059a176d06f7995b021fa5fb2
Boris <boris.zlobos@gmail.com>
2023-05-12 09:49:54 -0400

    first coomit

---
+++ src/app/heroes/pages/hero-page/hero-page.component.ts
@@ -0,0 +1,37 @@
+import { Component, OnInit } from '@angular/core';
+import {ActivatedRoute, Router} from '@angular/router';
+import { switchMap} from 'rxjs';
+import {Hero} from '../../interfaces/hero.interface';
+import {HeroesService} from '../../services/heroes-service.service';
+
+@Component({
+  selector: 'app-hero-page',
+  templateUrl: './hero-page.component.html',
+  styles: [
+  ]
+})
+export class HeroPageComponent implements OnInit{
+  
+  public hero?: Hero
+
+  constructor(
+    private heroesService: HeroesService,
+    private activateRoute: ActivatedRoute,
+    private router: Router, 
+  ) {}
+
+  ngOnInit(): void {
+    this.activateRoute.params
+    .pipe(
+      switchMap( params => this.heroesService.getHeroById( params['id']))
+    ).subscribe( hero => {
+      if( !hero ) return this.router.navigate([ '/heroes/list' ])
+      this.hero = hero
+      return
+    })
+  }
+
+  goBack(): void {
+    this.router.navigateByUrl('heroes/list')
+  }
+}
---
+++ src/environments/environments.prod.ts
@@ -0,0 +1,3 @@
+export const environments = {
+  baseUrl: 'http://boris-zarate.com/api'
+}
